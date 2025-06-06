6b9d22f3f0c1c0e25fa5d283c15f642831c3070a
Alvin11David <alvin69david@gmail.com>
2025-06-06 15:25:21 +0300

    added the border radius

--- hotel_booking_app/lib/pages/home.dart
+++ hotel_booking_app/lib/pages/home.dart
@@ -20,40 +20,46 @@ class _HomePageState extends State<HomePage> {
               children: [
                 ClipRRect(
                   borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40), 
-                  bottomRight: Radius.circular(40)),
-                child: Image.asset('assets/images/home.jpg', 
-                width: MediaQuery.of(context).size.width,
-                height: 280,
-                fit: BoxFit.cover
-                ),
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
+                  ),
+                  child: Image.asset(
+                    'assets/images/home.jpg',
+                    width: MediaQuery.of(context).size.width,
+                    height: 280,
+                    fit: BoxFit.cover,
+                  ),
                 ),
                 Container(
-                  padding: EdgeInsets.only(top: 35, left: 5,),
+                  padding: EdgeInsets.only(top: 35, left: 5),
                   width: MediaQuery.of(context).size.width,
                   height: 250,
                   decoration: BoxDecoration(
                     color: const Color.fromARGB(59, 0, 0, 0),
                     borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40), 
-                    bottomRight: Radius.circular(40)),
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40),
+                    ),
                   ),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Row(
                         children: [
-                      Icon(Icons.location_on,
-                        color: Colors.white),
-                        SizedBox(width: 10),
-                        Text('Uganda, Kampala',
-                        style: AppWidget.whitetextstyle(20.0)
+                          Icon(Icons.location_on, color: Colors.white),
+                          SizedBox(width: 10),
+                          Text(
+                            'Uganda, Kampala',
+                            style: AppWidget.whitetextstyle(20.0),
+                          ),
+                        ],
                       ),
-                      ],
+                      SizedBox(height: 30),
+                      Text(
+                        "Hey Alvin! Tell us where you want to go",
+                        style: AppWidget.whitetextstyle(22),
                       ),
-                      SizedBox(height: 30,),
-                      Text("Hey Alvin! Tell us where you want to go", style: AppWidget.whitetextstyle(22)),
-                      SizedBox(height: 20,),
+                      SizedBox(height: 20),
                       Container(
                         padding: EdgeInsets.only(bottom: 5, top: 5),
                         margin: EdgeInsets.only(right: 20.0),
@@ -65,22 +71,24 @@ class _HomePageState extends State<HomePage> {
                         child: TextField(
                           decoration: InputDecoration(
                             border: InputBorder.none,
-                            prefixIcon: Icon(Icons.search,
-                            color: Colors.white),
+                            prefixIcon: Icon(Icons.search, color: Colors.white),
                             hintText: 'Search Places...',
-                            hintStyle: AppWidget.whitetextstyle(20)
-                          )
+                            hintStyle: AppWidget.whitetextstyle(20),
+                          ),
                         ),
                       ),
                     ],
-                    )
+                  ),
                 ),
               ],
             ),
-            SizedBox(height: 20,),
+            SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.only(left: 10),
-              child: Text("The most relevant", style: AppWidget.headlinetextstyle(22)),
+              child: Text(
+                "The most relevant",
+                style: AppWidget.headlinetextstyle(22),
+              ),
             ),
             SizedBox(height: 20),
             Container(
@@ -89,14 +97,25 @@ class _HomePageState extends State<HomePage> {
                 scrollDirection: Axis.horizontal,
                 children: [
                   Container(
-                    child: Image.asset("assets/images/hotel1.jpg", width: MediaQuery.of(context).size.width/1.5,)
-                  )
+                    margin: EdgeInsets.only(left: 15),
+                    child: Column(
+                    children: [
+                     ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/images/hotel1.jpg',
+                        width: MediaQuery.of(context).size.width / 1.5,
+                      ),
+                    ),
+                    ]
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 }
