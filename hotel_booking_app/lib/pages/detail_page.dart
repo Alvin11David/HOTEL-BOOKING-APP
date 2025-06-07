a452944512c39c5fa4c5dfa1ed3732411d8c9149
Alvin11David <alvin69david@gmail.com>
2025-06-07 07:39:41 +0300

    added the icon

--- hotel_booking_app/lib/pages/detail_page.dart
+++ hotel_booking_app/lib/pages/detail_page.dart
@@ -14,18 +14,34 @@ class _DetailPageState extends State<DetailPage> {
       body: Container(
         child: Column(
           children: [
-            Stack(children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height/2.5,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
-                  child: Image.asset("assets/images/hotel1.jpg", fit: BoxFit.cover)),
-              )
-            ],)
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.5,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                    child: Image.asset(
+                      "assets/images/hotel1.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20, left: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.black),
+                    child: Icon(Icons.arrow_back,
+                    color: Colors.white),
+                )
+              ],
+            ),
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 }
