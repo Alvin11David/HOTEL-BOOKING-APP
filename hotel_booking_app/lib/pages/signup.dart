bd3b9da8529553af555b1ada4d033a49409b0572
Alvin11David <alvin69david@gmail.com>
2025-06-08 01:11:07 +0300

    added the icon

--- hotel_booking_app/lib/pages/signup.dart
+++ hotel_booking_app/lib/pages/signup.dart
@@ -13,65 +13,88 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        margin: EdgeInsets.only(top: 40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Image.asset("images/signup.png",
-              height: 300,
-              width: 300,
-              fit: BoxFit.cover
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(top: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset("images/signup.png",
+                height: 300,
+                width: 300,
+                fit: BoxFit.cover
+                ),
               ),
-            ),
-            SizedBox(height: 5,),
-            Center(child: Text("Sign Up", style: AppWidget.headlinetextstyle(25),)),
-            SizedBox(height: 5,),
-            Center(child: Text("Please enter the details to continue.", style: AppWidget.normaltextstyle(20))),
-            SizedBox(height: 10,),
-            Padding(
-              padding: const EdgeInsets.only(left: 30),
-              child: Text("Name", style: AppWidget.headlinetextstyle(20)),
-            ),
-            SizedBox(height: 10,),
-            Container(
-              margin: EdgeInsets.only(left:30, right: 30),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Color(0xffececf8),
+              SizedBox(height: 5,),
+              Center(child: Text("Sign Up", style: AppWidget.headlinetextstyle(25),)),
+              SizedBox(height: 5,),
+              Center(child: Text("Please enter the details to continue.", style: AppWidget.normaltextstyle(20))),
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Text("Name", style: AppWidget.headlinetextstyle(20)),
               ),
-              child: TextField(
-                decoration: InputDecoration(border: InputBorder.none,
-                prefixIcon: Icon(Icons.person,
-                color: const Color.fromARGB(255, 2, 77, 138)),
-                hintText: "Enter Your Name",
-                hintStyle: AppWidget.normaltextstyle(18)
+              SizedBox(height: 10,),
+              Container(
+                margin: EdgeInsets.only(left:30, right: 30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color(0xffececf8),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(border: InputBorder.none,
+                  prefixIcon: Icon(Icons.person,
+                  color: const Color.fromARGB(255, 2, 77, 138)),
+                  hintText: "Enter Your Name",
+                  hintStyle: AppWidget.normaltextstyle(18)
+                  )
+                )
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Text("Email", style: AppWidget.headlinetextstyle(20)),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                margin: EdgeInsets.only(left:30, right: 30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color(0xffececf8),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(border: InputBorder.none,
+                  prefixIcon: Icon(Icons.email,
+                  color: const Color.fromARGB(255, 2, 77, 138)),
+                  hintText: "Enter Your Email",
+                  hintStyle: AppWidget.normaltextstyle(18)
+                  )
+                )
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Text("Password", style: AppWidget.headlinetextstyle(20)),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                margin: EdgeInsets.only(left:30, right: 30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color(0xffececf8),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(border: InputBorder.none,
+                  prefixIcon: Icon(Icons.lock,
+                  color: const Color.fromARGB(255, 2, 77, 138)),
+                  hintText: "Enter Your Password",
+                  hintStyle: AppWidget.normaltextstyle(18)
+                  )
                 )
               )
-            ),
-            SizedBox(height: 10,),
-            Padding(
-              padding: const EdgeInsets.only(left: 30),
-              child: Text("Email", style: AppWidget.headlinetextstyle(20)),
-            ),
-            SizedBox(height: 10,),
-            Container(
-              margin: EdgeInsets.only(left:30, right: 30),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Color(0xffececf8),
-              ),
-              child: TextField(
-                decoration: InputDecoration(border: InputBorder.none,
-                prefixIcon: Icon(Icons.email,
-                color: const Color.fromARGB(255, 2, 77, 138)),
-                hintText: "Enter Your Email",
-                hintStyle: AppWidget.normaltextstyle(18)
-                )
-              )
-            )
-          ],
+            ],
+          ),
         ),
       )
     );
