f169a8e1c57c277635b8928e70f2e08e01808c94
Alvin11David <alvin69david@gmail.com>
2025-06-08 00:55:01 +0300

    added the icon

--- hotel_booking_app/lib/pages/signup.dart
+++ hotel_booking_app/lib/pages/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotel_booking_app/services/widget_support.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -11,9 +12,30 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
+        margin: EdgeInsets.only(top: 40),
         child: Column(
-          children: [],
+          children: [
+            Center(
+              child: Image.asset("images/signup.png",
+              height: 300,
+              width: 300,
+              fit: BoxFit.cover
+              ),
+            ),
+            SizedBox(height: 5,),
+            Text("Sign Up", style: AppWidget.headlinetextstyle(25),),
+            SizedBox(height: 5,),
+            Text("Please enter the details to continue.", style: AppWidget.normaltextstyle(20)),
+            Container(
+              margin: EdgeInsets.only(left:20, right: 20),
+              decoration: BoxDecoration(
+                color: Color(0xffececf8),
+              ),
+              child: TextField()
+            )
+          ],
         ),
       )
     );
