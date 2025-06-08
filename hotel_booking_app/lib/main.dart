0465f8b99322dfb041c870986b9e49c412108ed9
Alvin11David <alvin69david@gmail.com>
2025-06-08 10:50:56 +0300

    initialized

--- hotel_booking_app/lib/main.dart
+++ hotel_booking_app/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_booking_app/pages/bottomnav.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
