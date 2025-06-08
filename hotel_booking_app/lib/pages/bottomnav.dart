bf71157df2920b956ce478cedd32250ee6c67b97
Alvin11David <alvin69david@gmail.com>
2025-06-08 10:08:58 +0300

    added the icon

---
+++ hotel_booking_app/lib/pages/bottomnav.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_booking_app/pages/home.dart';      
+import 'package:hotel_booking_app/pages/booking.dart';   
+import 'package:hotel_booking_app/pages/profile.dart';   
+import 'package:hotel_booking_app/pages/wallet.dart'; 
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';   
+
+class Bottomnav extends StatefulWidget {
+  const Bottomnav({super.key});
+
+  @override
+  State<Bottomnav> createState() => _BottomnavState();
+}
+
+class _BottomnavState extends State<Bottomnav> {
+  late List<Widget> pages;
+
+  late HomePage homePage;
+  late BookingPage booking;
+  late ProfilePage profile;
+  late WalletPage wallet;
+
+  int currentTabIndex = 0;
+
+  @override
+  void initState() {
+    homePage = HomePage();
+    booking = BookingPage();
+    profile = ProfilePage();
+    wallet = WalletPage();
+
+    pages = [homePage, booking, wallet, profile];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 70,
+        backgroundColor: Colors.white,
+        color: Colors.black,
+        animationDuration: Duration(milliseconds: 500),
+        onTap: (int index) {
+        setState(() {
+          currentTabIndex = index;
+        });
+      },
+      items: [
+        Icon(Icons.home, color: Colors.white, size: 30),
+        Icon(Icons.shopping_bag, color: Colors.white, size: 30),
+        Icon(Icons.wallet, color: Colors.white, size: 30),
+        Icon(Icons.person, color: Colors.white, size: 30),
+      ],
+    ),
+    body: pages[currentTabIndex],
+    );
+  }
+}
--- hotel_booking_app/lib/pages/signin.dart
+++ hotel_booking_app/lib/pages/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotel_booking_app/pages/home.dart';
 import 'package:hotel_booking_app/pages/signup.dart';
 import 'package:hotel_booking_app/services/widget_support.dart';
 
@@ -20,7 +21,7 @@ class _SignInPageState extends State<SignInPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Center(
-                child: Image.asset("images/login.png",
+                child: Image.asset("assets/images/login.png",
                     height: 300,
                     width: 300,
                     fit: BoxFit.cover),
@@ -82,20 +83,25 @@ class _SignInPageState extends State<SignInPage> {
                   ),
                 ),
               SizedBox(height: 30,),
-              Center(
-                child: Container(
-                  height: 60,
-                  margin: EdgeInsets.only(left:30, right: 30),
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 2, 77, 138),
-                    borderRadius: BorderRadius.circular(20)
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                },
+                child: Center(
+                  child: Container(
+                    height: 60,
+                    margin: EdgeInsets.only(left:30, right: 30),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 2, 77, 138),
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    width: MediaQuery.of(context).size.width/2.0,
+                    child: Center(child: Text("Sign In", style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    )))
                   ),
-                  width: MediaQuery.of(context).size.width/2.0,
-                  child: Center(child: Text("Sign In", style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                  )))
                 ),
               ),
               SizedBox(height: 20,),
